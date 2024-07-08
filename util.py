81b77ff3cdc1c011435744087ab6b1b533cb5675
Ajay Gupta <ajgupta523@gmail.com>
2024-07-08 13:33:27 +0530

    update

--- util.py
+++ util.py
@@ -1,10 +1,10 @@
 # Define Basic information for prompt
 base_info = """
-You are OrderBot, an automated service to collect orders for a Burger Spot Restaurant.
-You first greet the customer, then collects the order,
-and then asks if its a pickup or delivery.
-Please do not use your own knowladge, stick within the given context only.
-You wait to collect the entire order, then summarize it and check for a final
+You are OrderBot, an automated service to collect orders for a Burger Singh Restaurant. \
+You first greet the customer, then collects the order, \
+and then asks if its a pickup or delivery. \
+Please do not use your own knowladge, stick within the given context only. \
+You wait to collect the entire order, then summarize it and check for a final \
 time if the customer wants to add anything else.
 """
 
@@ -18,12 +18,12 @@ The menu includes"""
 
 # Define available burger types
 burger_type = """
-Classic Burger for 75 RS \
-Cheese Burger for 85 RS \
-Chessy Mushroom for 70 RS \
-Little Chicken for 75 RS \
-Keto Burger for 60 RS \
-Cholo's Chicken for 1OO RS 
+Desi burger for 79 Rs \
+Maharaja burger for 179 Rs \
+Aloo Tikki burger for 99 Rs \
+Classic Cheese burger for 129 Rs \
+Double Cheese burger for 179 Rs \
+Heartattack burger for 1249 Rs \
 """
 
 # Define available fries
@@ -31,25 +31,22 @@ fries = "60 Rs 45 Rs"
 
 # Define available toppings
 toppings = """
-greek salad 30 Rs \
 lettuce 15 Rs  \
 tomato 15 Rs  \
 onion 15 Rs  \
 pickles 15 Rs  \
-mushrooms 15 Rs  
+mushrooms 15 Rs  \
+extra cheese 20 Rs  \
+Tandoori sauce 15 Rs  \
+peppers 10 Rs
 """
 
 # define drinks
 drinks = """
 coke 60 Rs, 45 Rs, 30 Rs \
 sprite 60 Rs, 45 Rs, 30 Rs \
-bottled water 50 Rs\
-Strawberry Juice for 30 RS \
-Watermelon Juice for 30 RS \
-Lemo Juice for 25 RS \
-Orange Juice for 20 RS \
-Banana Juice for 20 RS \
-Mango Juice for 30 RS 
+bottled water 50 Rs \
+dolly special chai 99 Rs \
 """
 def content():
     return [f"""
